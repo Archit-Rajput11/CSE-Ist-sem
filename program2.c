1093428d95f961bcad774a78795283d49aedd628
Archit-Rajput11 <2025A1R047@mietjammu.in>
2025-10-03 19:15:28 +0530

    Update and rename program2.c to odd_numbers

--- program2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int x = 13;
-    char ch ='y';
-    float y = 1.33;
-    double z=109.333;
-    printf("Value of x= %d \nValue of ch= %c \nVlaue of y= %f \nValue of z = %Lf ",x,ch,y,z);
-    return 0;
-}
