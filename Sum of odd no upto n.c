7a2c3bd802ef024728867837a148856500ad5293
Archit-Rajput11 <2025A1R047@mietjammu.in>
2025-10-05 18:34:20 +0530

    Create Sum of odd no upto n.c

---
+++ Sum of odd no upto n.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n,i,sum=0;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i+=2){
+        sum+=i;
+    }
+    printf("%d",sum);
+    return 0;
+}
