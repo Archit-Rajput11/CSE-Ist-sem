118bc4d03158dfcc099fa19b2171844a88a47247
Archit-Rajput11 <2025A1R047@mietjammu.in>
2025-10-09 19:50:26 +0530

    Update WAP to calculate electricity bill.c

--- WAP to calculate electricity bill.c
+++ WAP to calculate electricity bill.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 int main() {
-    int u;
-    float bill=0.0;
-    scanf("%d",&u);
-    if(u<=50){
-        bill=0.50*u;
+    int units;
+    float bill = 0.0;
+
+    scanf("%d", &units);
+
+    if (units <= 50) {
+        bill = units * 0.50;
+    } else if (units <= 150) {
+        bill = (50 * 0.50) + ((units - 50) * 0.75);
+    } else if (units <= 250) {
+        bill = (50 * 0.50) + (100 * 0.75) + ((units - 150) * 1.20);
+    } else {
+        bill = (50 * 0.50) + (100 * 0.75) + (100 * 1.20) + ((units - 250) * 1.50);
     }
-    else if(u>=51 && u<=150){
-        bill=(50 * 0.50) + ((units - 50) * 0.75);
-    }
-    else if(u>=151 && u<=250){
-        (50 * 0.50) + (100 * 0.75) + ((units - 150) * 1.20);
-    }
-    else{
-        bill=(50 * 0.50) + (100 * 0.75) + (100 * 1.20) + ((units - 250) * 1.50);
-    }
-    bill+= bill*0.20;
-    printf("%.2f",bill);
+
+    // Add 20% surcharge
+    bill += bill * 0.20;
+
+    // Print the final bill up to 2 decimal places
+    printf("%.2f\n", bill);
+
     return 0;
 }
+
